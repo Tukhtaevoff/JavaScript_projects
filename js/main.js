48a6baef1daf089ed570a96589ed7fb79520b624
Tukhtaev27 <108573349+Tukhtaev27@users.noreply.github.com>
2022-09-09 16:05:19 +0500

    one task added

--- js/main.js
+++ js/main.js
@@ -9,15 +9,18 @@ const textEl = document.querySelector(".text");
 // textEl.innerHTML = `<br> ${capitalize}`;
 
 // Task 2
-const swapCase = string => {
-    let arrEl = string.split(' ');
-    const newFunction = arrEl.map(( word, word2 ) => word2 % 2 === 0 ? word.toUpperCase() : word);
-    return newFunction
-}
-textEl.innerHTML = swapCase("swap case example and hello world");
+// const swapCase = string => {
+//     let arrEl = string.split(' ');
+//     const newFunction = arrEl.map(( word, word2 ) => word2 % 2 === 0 ? word.toUpperCase() : word);
+//     return newFunction
+// }
+// textEl.innerHTML = swapCase("swap case example and hello world");
 
 // Task 3
-
+// const shiftedLetters = function(string) {
+//     return string.substring(1);
+// }
+// textEl.textContent = shiftedLetters("Hello");
 
 // Task 4
 
@@ -26,6 +29,3 @@ textEl.innerHTML = swapCase("swap case example and hello world");
 
 
 // Task 6
-
-
-
