7d15a31281a93a88ddc9ad21c6f87c86e985cdaf
Tukhtaev27 <108573349+Tukhtaev27@users.noreply.github.com>
2022-09-11 14:38:36 +0500

    hometask 3 added

--- js/main.js
+++ js/main.js
@@ -1,31 +1,52 @@
-const textEl = document.querySelector(".text");
-// Task 1
-// let wordEl = [
-//     "Hello world hello world",
-//     "Come on come on come on",
-//     "Oh my god omg and something"
-// ];
-// const capitalize = wordEl.map(string => string.toUpperCase());
-// textEl.innerHTML = `<br> ${capitalize}`;
+const inputName = document.getElementById("inputName");
+const inputSurname = document.getElementById("inputSurname");
+const inputAge = document.getElementById("inputAge");
+const inputEmail = document.getElementById("inputEmail");
+const inputNumber = document.getElementById("inputNumber");
 
-// Task 2
-// const swapCase = string => {
-//     let arrEl = string.split(' ');
-//     const newFunction = arrEl.map(( word, word2 ) => word2 % 2 === 0 ? word.toUpperCase() : word);
-//     return newFunction
-// }
-// textEl.innerHTML = swapCase("swap case example and hello world");
-
-// Task 3
-// const shiftedLetters = function(string) {
-//     return string.substring(1);
-// }
-// textEl.textContent = shiftedLetters("Hello");
-
-// Task 4
+const showButton = document.getElementById("showButton");
+const formEl = document.getElementById("form")
+const lsOutput = document.getElementById("lsOutput")
+const containerEl = document.querySelector(".container")
+const removeBtn = document.querySelector(".remove");
 
 
-// Task 5
+let confirmEl = confirm('Are you going to begin?');
+
+if (confirmEl) {
+    alert("Do you really want to begin?");
+}
+
+showButton.addEventListener("click", function() {
+    document.body.style.transition = "2s";
+    document.body.style.backgroundColor = "#1d2b3a";
+    containerEl.style.transition = "2s"
+    containerEl.style.display = "block";
+    showButton.style.transition = "2s";
+    showButton.style.transform = "translateX(-300px)";
+    
+})
+formEl.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const nameEl = inputName.value;
+    const surnameEl = inputSurname.value;
+    const ageEl = inputAge.value;
+    const emailEl = inputEmail.value;
+    const numberEl = inputNumber.value;
+
+    localStorage.setItem("Name", nameEl);
+    localStorage.setItem("Surname", surnameEl);
+    localStorage.setItem("Age", ageEl);
+    localStorage.setItem("Email", emailEl);
+    localStorage.setItem("Number", numberEl);
+    
+    if (ageEl <= 0 || ageEl > 120) {
+        alert("Sorry, It's impossible to be at the age of" + " " + ageEl);
+    }
 
 
-// Task 6
+    lsOutput.innerHTML += `Your name is ${nameEl} <br /> Your surname is ${surnameEl}  <br /> Your are ${ageEl} old  <br /> Your email is ${emailEl}  <br /> Your number is ${numberEl} <br>`;
+
+    formEl.reset();
+})
+
