a4d9a245c9b83250ca4aa34a77df65b5295864cf
Tukhtaev27 <108573349+Tukhtaev27@users.noreply.github.com>
2022-09-20 04:36:45 +0500

    Search bar added

--- js/main.js
+++ js/main.js
@@ -1,68 +1,162 @@
-const formEl = document.getElementById("form");
+const cars = [
+    {
+        name: "Lamborghini",
+        img: "./images/lamborghini-removebg-preview.png",
+        price: 750000,
+        desc: `Lorem ipsum dolor sit amet.`
+    },
+    {
+        name: "Bugatti", 
+        img: "./images/bugatti-removebg-preview.png",
+        price: 3500000,
+        desc: `Lorem ipsum dolor sit amet.`
+    },
+    {
+        name: "Mercedes", 
+        img: "./images/mercedes-removebg-preview.png",
+        price: 60000,
+        desc: `Lorem ipsum dolor sit amet.`
+    },
+    {
+        name: "Bugatti", 
+        img: "./images/bugatti2-removebg-preview.png",
+        price: 2500000,
+        desc: `Lorem ipsum dolor sit amet.`
+    },
+    {
+        name: "Lamborghini", 
+        img: "./images/lamborghini-2.webp",
+        price: 1500000,
+        desc: `Lorem ipsum dolor sit amet.`
+    },
+    {
+        name: "Mercedes", 
+        img: "./images/mercedes2-removebg-preview.png",
+        price: 150000,
+        desc: `Lorem ipsum dolor sit amet.`
+    },
+];
 
-const info = [];
-
-let listEl = document.querySelector(".list");
-
-formEl.addEventListener("submit", evt => {
-  evt.preventDefault();
-
-  const elements = event.target.elements;
-  const nameInputVal = elements.name.value;
-  const lastnameInputVal = elements.lastname.value;
-  const emailInputVal = elements.email.value;
-  const passwordInputVal = elements.password.value;
-
-    if (nameInputVal && lastnameInputVal && emailInputVal && passwordInputVal) {
-      const newObj = {
-        name: nameInputVal,
-        lastName: lastnameInputVal,
-        email: emailInputVal,
-        password: passwordInputVal
-      }
-      info.push(newObj);
-    }
-
-    let displayUI = ``;
-  info.map(value => {
-    
-      displayUI += `
-      <li class="list__item bg-primary text-white">
-      <b>${value.name}</b>
-      <span>${value.lastName}</span>
-      <p>${value.email}</p>
-      <em>${value.password}</em>
-      </li>
-      `
-  });
-
-  listEl.innerHTML = displayUI;
-
-  formEl.reset();
-})
-
+const allBtn = document.getElementById("all");
+const sortFunc = document.getElementById("sort");
+const filterBtn = document.getElementById("filter-btn");
 const searchForm = document.getElementById("search-form");
 
-searchForm.addEventListener("submit", evt => {
-  evt.preventDefault();
+const listEl = document.getElementById("list");
 
-  const searchInputVal = evt.target.elements.search.value;
+cars.map(function (item) {
+    let displayUI = ``;
+    displayUI = `
+    <li class="list__item">
+    <h2 class="name">${item.name}</h2>
+    <img class="img" src="${item.img}">
+    <p>${item.desc}</p>
+    <strong>Price: ${item.price} $</strong>
+    </li>
+    `
+    listEl.innerHTML += displayUI;
+})
 
-  let displayUI = ``;
+filterBtn.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    let displayUI = cars.map(function (item) {
+        if (item.name == "Bugatti") {
+            return `
+            <li class="list__item">
+            <h2 class="name">${item.name}</h2>
+            <img class="img" src="${item.img}">
+            <p>${item.desc}</p>
+            <strong>Price: ${item.price} $</strong>
+            </li>
+            `
+        }
+    })
+    displayUI = displayUI.join("");
+    listEl.innerHTML = displayUI;
+})
 
-  info.map(value => {
-      if (searchInputVal == value.name) {
-        displayUI += `
-        <li class="list__item bg-primary text-white">
-        <b>${value.name}</b>
-        <span>${value.lastName}</span>
-        <p>${value.email}</p>
-        <em>${value.password}</em>
+allBtn.addEventListener("click", function () {
+    let displayUI = cars.map(function (item) {
+        return `
+        <li class="list__item">
+        <h2 class="name">${item.name}</h2>
+        <img class="img" src="${item.img}">
+        <p>${item.desc}</p>
+        <strong>Price: ${item.price} $</strong>
         </li>
         `
-      }
-  })
-  listEl.innerHTML = displayUI;
-
-  searchForm.reset();
+    })
+    displayUI = displayUI.join("");
+    listEl.innerHTML = displayUI;
 })
+
+sortFunc.addEventListener("change", function (evt) {
+    let displayUI = ``;
+    switch(this.value) {
+        case "Bugatti":
+            cars.map(function (item) {
+                if (item.name === "Bugatti") {
+                    displayUI += `
+                    <li class="list__item">
+                    <h2 class="name">${item.name}</h2>
+                    <img class="img" src="${item.img}">
+                    <p>${item.desc}</p>
+                    <strong>Price: ${item.price} $</strong>
+                    </li>
+                    `
+                    listEl.innerHTML = displayUI;
+                }
+            })
+        case "Mercedes":
+            cars.map(function (item) {
+                if (item.name === "Mercedes") {
+                    displayUI += `
+                    <li class="list__item">
+                    <h2 class="name">${item.name}</h2>
+                    <img class="img" src="${item.img}">
+                    <p>${item.desc}</p>
+                    <strong>Price: ${item.price} $</strong>
+                    </li>
+                    `
+                    listEl.innerHTML = displayUI;
+                }
+            })
+        case "Lamborghini":
+            cars.map(function (item) {
+                if (item.name === "Lamborghini") {
+                    displayUI += `
+                    <li class="list__item">
+                    <h2 class="name">${item.name}</h2>
+                    <img class="img" src="${item.img}">
+                    <p>${item.desc}</p>
+                    <strong>Price: ${item.price} $</strong>
+                    </li>
+                    `
+                    listEl.innerHTML = displayUI;
+                }
+            })
+        default: 
+           break;
+    }
+})
+
+searchForm.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    const inputValue = evt.target.elements.search.value;
+
+    let displayUI = cars.map(function (item) {
+        if (inputValue === item.name) {
+          return `
+          <li class="list__item">
+          <h2 class="name">${item.name}</h2>
+          <img class="img" src="${item.img}">
+          <p>${item.desc}</p>
+          <strong>Price: ${item.price} $</strong>
+          </li>
+          `
+        }
+    })
+    displayUI = displayUI.join("");
+    listEl.innerHTML = displayUI;
+})
+
